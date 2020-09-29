b41fbb74f3a3430b1b356bacb829c63dd64ac57c
David Morais <morais.david@live.com>
2020-09-29 08:30:06 -0300

    Testando restore

--- pg_restore.py
+++ pg_restore.py
@@ -44,17 +44,12 @@ def get_remote_db_credentials():
 
 def do_pg_restore(host, user, passwd, datname, dump_file):
 
-    pg_restore_command = [
-        'pg_restore',
-        f'-U {user}',
-        f'--no-password',
-        f'-h {host}',
-        f'-j 2',
-        '--no-owner',
-        f'-Fd',
-        f'-d {datname}',
-        dump_file
-    ]
+    pg_restore_command = f'pg_restore --host={host} ' \
+            f'--dbname={datname} ' \
+            f'--username={user} ' \
+            f'--no-password ' \
+            f'--format=d ' \
+            f'--file={dump_file} '
 
     print(pg_restore_command)
 
@@ -82,10 +77,11 @@ def main():
     address = credentials['address']
     password = credentials['password']
 
-    dump_file = 'sapiencia'
+    dump_file_path = 'sapiencia'
     print(credentials)
     # print('Restaurando dump em na nova instancia')
-    do_pg_restore(address, user, password, database, dump_file)
+    restore_success = do_pg_restore(address, user, password, database, dump_file)
+    print(restore_success)
 
 
 if __name__ == "__main__":
